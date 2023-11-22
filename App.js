0d1282213cef99327e9473e55a735c8db13bfed6
anantvikramsingh-sm <anant.s@softwaremathematics.com>
2023-11-22 09:34:17 +0530

    app.js

--- App.js
+++ App.js
@@ -1,10 +1,17 @@
 import { View } from "react-native";
+import * as React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
 import Login from "./screen/Login";
+import Reset from "./screen/ResetPassword";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import NavApp from "./component/NavApp";
 
+const Stack=createNativeStackNavigator();
 export default function App(){
   return(
-    <View>
-    <Login/>
-    </View>
+    <NavigationContainer>
+      <NavApp/>
+    </NavigationContainer>
+    
   )
 }
